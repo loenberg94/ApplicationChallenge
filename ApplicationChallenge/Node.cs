7e91942442b3ee9273b2dbf64449a16824cef709
Magnus Ameland Lønberg <loenberg48@gmail.com>
2019-02-09 23:43:38 +0100

    Working: * Tree traversal * Ability to find max path
    Missing:
    * String input to Tree

---
+++ ApplicationChallenge/Node.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationChallenge
+{
+    class Node
+    {
+        private static int ID = 0;
+        public int Id { get; } // Used to memoize
+
+        internal Node Left { get; }
+        internal Node Right { get; }
+        internal Node Parent { get; set; }
+        public int BestValue { get; set; } = int.MinValue;
+        public readonly int Value;
+        public int Depth { get; set; } = 0;
+
+        public Node(Node left, int value, Node right) // Node constructor
+        {
+            Left = left;
+            Right = right;
+            Value = value;
+            Id = ID;
+            ID++;
+        }
+
+        public Node(int value) // Leaf constructor
+        {
+            Value = value;
+            Id = ID;
+            ID++;
+        }
+
+        public bool IsEven()
+        {
+            return Value % 2 == 0;
+        }
+
+        public bool IsLeaf()
+        {
+            return Left == null && Right == null;
+        }
+
+        public int[] GetPath()
+        {
+            int[] path = new int[Depth + 1];
+
+            Node tmp = this;
+            while (tmp != null)
+            {
+                path[tmp.Depth] = tmp.Value;
+                tmp = tmp.Parent;
+            }
+
+            return path;
+        }
+
+        public int GetValue()
+        {
+            int sum = 0;
+
+            Node tmp = this;
+            while (tmp != null)
+            {
+                sum += tmp.Value;
+                tmp = tmp.Parent;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ ApplicationChallenge/Triangle.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationChallenge
+{
+    class Triangle
+    {
+        private Node root;
+        private int numberOfNodes;
+
+        public Triangle(string triangle)
+        {
+
+        }
+
+        private bool IsValidPath(Node current,Node prev)
+        {
+            return prev == null || prev.IsEven() != current.IsEven();
+        }
+        
+        private Node MaxPath(Node[] vNodes,Node prev, Node current, int acc, int depth)
+        {
+            current.Depth = depth;
+            
+            if (IsValidPath(current, prev))
+            {
+                if (current.Parent == null) // Unvisited node
+                {
+                    current.Parent = prev;
+                    current.BestValue = acc + current.Value;
+
+                    if (current.IsLeaf())
+                    {
+                        vNodes[current.Id] = current;
+                        return current;
+                    }
+
+                    Node lPath = MaxPath(vNodes, current, current.Left, acc + current.Value, depth + 1);
+                    Node rPath = MaxPath(vNodes, current, current.Right, acc + current.Value, depth + 1);
+                    
+                    bool goLeft = lPath.BestValue > rPath.BestValue;
+                    vNodes[current.Id] = goLeft ? lPath:rPath;
+                    return goLeft ? lPath : rPath;
+                }
+                else // Already visited node
+                {
+                    if (current.Parent.BestValue < prev.BestValue)
+                    {
+                        int diff = prev.BestValue - current.Parent.BestValue;
+                        current.Parent = prev;
+                        current.BestValue = current.BestValue + diff;
+                    }
+                    return vNodes[current.Id];
+                }
+            }
+            return new Node(int.MinValue);
+        }
+        
+        public Node MaxPath(Node tree = null, int nodes = -1)
+        {
+            //return MaxPathDP(new Path[numberOfNodes], root, 0, 0);
+            return MaxPath(new Node[nodes==-1?numberOfNodes:nodes],null,tree??root,0, 0);
+        }
+    }
+}
